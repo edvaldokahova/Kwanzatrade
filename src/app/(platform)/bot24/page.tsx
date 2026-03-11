52fdba9c063924c4892e99750c9fa6a9a8caa040
edvaldokahova <edvaldocahova@gmail.com>
2026-03-12 00:05:24 +0100

    Update page.tsx

--- src/app/(platform)/bot24/page.tsx
+++ src/app/(platform)/bot24/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { supabase } from "@/lib/supabaseClient";
-import { Fire } from "lucide-react";
+import { Flame } from "lucide-react";
 
 type Bot24HistoryItem = {
   id: string;
