0f7deded376ce761d9efc5d2cdaa6adf3bc5c486
edvaldokahova <edvaldocahova@gmail.com>
2026-04-14 23:33:49 +0100

    Update layout.tsx

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -70,7 +70,7 @@ export default function PlatformLayout({
       <div className="flex-1 flex flex-col min-h-screen">
         <Navbar setSidebarOpen={setSidebarOpen} />
         {/* ✅ pt-16 para compensar a Navbar fixed */}
-        <main className="flex-1 pt-16 px-0 overflow-y-auto w-full">
+        <main className="flex-1 mt-16 px-0 overflow-y-auto w-full bg-[#0b0b0c]">
           {children}
         </main>
       </div>
